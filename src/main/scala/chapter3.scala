8a5105f89eefab8d37a3697ac4a2c3e1643cb5d0
Paul Dudenkov <nocs00@gmail.com>
2018-04-27 13:03:39 +0300

    chapter3: lists

--- src/main/scala/chapter3.scala
+++ src/main/scala/chapter3.scala
@@ -1,5 +1,7 @@
 package chapter3 {
 
+  import scala.annotation.tailrec
+
   sealed trait List[+A]
 
   case object Nil extends List[Nothing]
@@ -52,6 +54,112 @@ package chapter3 {
       case Cons(h, Nil) => Nil
       case Cons(h, t) => Cons(h, init(t))
     }
+
+    def foldRight[A, B](l: List[A], z: B)(f: (A, B) => B): B = l match {
+      case Nil => z
+      case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+    }
+
+    def sum2(l: List[Int]) =
+      foldRight(l, 0.0)(_ + _)
+
+    def product2(l: List[Double]) =
+      foldRight(l, 1.0)(_ * _)
+
+    def length[A](l: List[A]): Int =
+      foldRight(l, 0)((_, b) => 1 + b)
+
+    @tailrec
+    def foldLeft[A, B](l: List[A], z: B)(f: (B, A) => B): B = l match {
+      case Nil => z
+      case Cons(x, xs) => foldLeft(xs, f(z, x))(f)
+    }
+
+    def sum3(l: List[Int]) = foldLeft(l, 0.0)(_ + _)
+
+    def product3(l: List[Double]) = foldLeft(l, 1.0)(_ * _)
+
+    def append[A](a1: List[A], a2: A): List[A] = a1 match {
+      case Nil => Cons(a2, Nil)
+      case Cons(h, t) => Cons(h, append(t, a2))
+    }
+
+    def reverse[A](l: List[A]): List[A] = l match {
+      case Nil => Nil
+      case Cons(x, xs) => append(reverse(xs), x)
+    }
+
+    def reverseWithFold[A](l: List[A]): List[A] = foldLeft(l, Nil: List[A])((b, a) => Cons(a, b))
+
+    //    def append2[A](a1: List[A], a2: List[A]): List[A] = {
+    //      foldLeft(a1, a2)((b,a) => a match {
+    //          case Cons(h, Nil) =>
+    //          case _ => a
+    //        })
+    //
+    //    }
+
+    def flatten[A](l: List[List[A]]): List[A] = l match {
+      case Nil => Nil
+      case Cons(h, t) => append(h, flatten(t))
+    }
+
+    def map[A, B](l: List[A])(f: A => B): List[B] = l match {
+      case Nil => Nil
+      case Cons(h, t) => Cons(f(h), map(t)(f))
+    }
+
+    def filter[A](l: List[A])(f: A => Boolean): List[A] = l match {
+      case Nil => Nil
+      case Cons(h, t) if f(h) => Cons(h, filter(t)(f))
+      case Cons(h, t) => filter(t)(f)
+    }
+
+    def flatMap[A, B](l: List[A])(f: A => List[B]): List[B] = l match {
+      case Nil => Nil
+      case Cons(h, t) => append(f(h), flatMap(t)(f))
+    }
+
+    def headOption[A](l: List[A]): Option[A] = l match {
+      case Nil => None
+      case Cons(h, t) => Some(h)
+    }
+
+    def merge[A](l1: List[A], l2: List[A])(f: (A, A) => A): List[A] = l1 match {
+      case Nil => l2
+      case Cons(h, t) => Cons(headOption(l2).map(f(_, h)).getOrElse(h), merge(t, tail(l2))(f))
+    }
+
+    def merge2[A](list1: List[A], list2: List[A])(mergeFunction: (A, A) => A): List[A] = {
+
+      def go(list1: List[A], list2: List[A]): List[A] = {
+        val value1 = headOption(list1)
+        val value2 = headOption(list2)
+
+        val mergedOption = value1.flatMap(v1 => value2.map(v2 => mergeFunction(v1, v2)))
+        mergedOption match {
+          case None => go(tail(list1), tail(list2))
+          case Some(mergedValue) => Cons(mergedValue, go(tail(list1), tail(list2)))
+        }
+      }
+
+      go(list1, list2)
+    }
+
+    def hasSubsequence[A](l: List[A], sub: List[A]): Boolean = {
+      def go(l: List[A], sub2: List[A]): Boolean = l match {
+        case Nil => sub2 == Nil
+        case Cons(h, t) => sub2 match {
+          case Nil => true
+          case Cons(hh, tt) =>
+            if (h == hh) go(t, tt)
+            else go(t, sub)
+        }
+      }
+
+      go(l, sub)
+    }
+
   }
 
   object Runner {
@@ -84,6 +192,31 @@ package chapter3 {
       println(s"${dropWhile(xs)(x => x > 3)}")
 
       println(s"${init(xs)}")
+
+      val ex8 = foldRight(List(1, 2, 3), Nil: List[Int])(Cons(_, _))
+      println(ex8)
+      println(length(List("a", 33.0, 3, 4, 5, 6, 7)))
+      println(foldLeft(List(1, 2, 3, 4, 5), 0)(_ + _))
+      println(append(List(1, 2, 3), 4))
+      println(reverse(List(1, 2, 3)))
+      println(reverseWithFold(List(1, 2, 3)))
+
+      println(flatten(List(List(1, 2, 3), List(5, 9, 0), List(-5, -9, 0))))
+      println(map(List(1, 2, 3))((x: Int) => x + 1))
+      println(map(List(1.1, 2.2, 3.3))((x: Double) => x.toString))
+      println(filter(List(1, 2, 3, 4, 5, 6))((x: Int) => x % 2 == 0))
+      println(flatMap(List(1, 2, 3))((x: Int) => List(x, x + 1, x + 2)))
+      println(merge(List(1, 2, 3), List(4, 5, 6))(_ + _))
+
+      println(hasSubsequence(List(1, 2, 3, 4, 5), List(1))) //true
+      println(hasSubsequence(List(1, 2, 3, 4, 5), List(1, 2))) //true
+      println(hasSubsequence(List(1, 2, 3, 4, 5), List(4, 5))) //true
+      println(hasSubsequence(List(1, 2, 3, 4, 5), List(5))) //true
+      println(hasSubsequence(List(1, 2, 3, 4, 5), List(5, 6))) //false
+      println(hasSubsequence(List(1, 2, 3, 4, 5), List(2, 3))) //true
+      println(hasSubsequence(List(1, 2, 3, 4, 5), List(2, 4))) //false
+      println(hasSubsequence(List(1, 2, 3, 4, 5), Nil)) //true
+      println(hasSubsequence(Nil, List(2, 3))) //false
     }
   }
 
